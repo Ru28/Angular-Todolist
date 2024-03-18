6c9ec8b20d9f4e9b746617c784dff8b3d53e4554
Rupesh Virani <rupeshvirani01@gmail.com>
2024-03-19 00:23:37 +0530

    add delete Task Functionality

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Task } from '../../Task';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import { CommonModule } from '@angular/common';
@@ -12,6 +12,11 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   styleUrl: './task-item.component.css'
 })
 export class TaskItemComponent {
-  @Input() task!: Task
+  @Input() task!: Task;
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter()
   faTimes = faTimes;
+
+  onDelete(task:Task) {
+    this.onDeleteTask.emit(task);
+  }
 }
