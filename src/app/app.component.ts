c9bcdcc9b22204d20cac7564035e9e34909ec7b7
Rupesh Virani <rupeshvirani01@gmail.com>
2024-03-19 02:02:55 +0530

    added Add Task Component & Forms Module and ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './components/header/header.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,TasksComponent],
+  imports: [RouterOutlet,FormsModule,HeaderComponent,TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
