23dfa3f1b7d2372d38f84fee278a040819c80da2
Rupesh Virani <rupeshvirani01@gmail.com>
2024-03-18 18:32:19 +0530

    Initial commit && add ng component for todo list

---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import { Task } from '../../Task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+@Component({
+  selector: 'app-task-item',
+  standalone: true,
+  imports: [FontAwesomeModule,CommonModule ],
+  templateUrl: './task-item.component.html',
+  styleUrl: './task-item.component.css'
+})
+export class TaskItemComponent {
+  @Input() task!: Task
+  faTimes = faTimes;
+}
