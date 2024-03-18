67249a3ece6acb09a7d6605f6d5942b2aee8c8f2
Rupesh Virani <rupeshvirani01@gmail.com>
2024-03-19 02:38:28 +0530

    Submit & Create Task

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Task } from '../../Task';
 @Component({
   selector: 'app-add-task',
   standalone: true,
@@ -8,8 +9,27 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './add-task.component.css'
 })
 export class AddTaskComponent {
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter()
   text!: string;
   day!: string;
   reminder: boolean = false;
   constructor() {}
+
+  onSubmit(){
+    if(!this.text){
+      alert('Please add a Task');
+      return;
+    }
+
+    const newTask = {
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder
+    }
+
+    this.onAddTask.emit(newTask);
+    this.text = '';
+    this.day = '';
+    this.reminder = false;
+  }
 }
