81607a56f08cb4c52cb879a0110d8b655ed61627
Rupesh Virani <rupeshvirani01@gmail.com>
2024-03-19 14:10:52 +0530

    add Toggle Button & UI services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './components/header/header.component';
 import { TasksComponent } from './components/tasks/tasks.component';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FormsModule,HeaderComponent,TasksComponent],
+  imports: [RouterOutlet,FormsModule,HeaderComponent,TasksComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,10 +1,13 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Task } from '../../Task';
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.css'
 })
@@ -13,7 +16,14 @@ export class AddTaskComponent {
   text!: string;
   day!: string;
   reminder: boolean = false;
-  constructor() {}
+  showAddTask!: boolean;
+  subscription: Subscription;
+
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+        .onToggle()
+        .subscribe((value)=>(this.showAddTask=value)); 
+  }
 
   onSubmit(){
     if(!this.text){
